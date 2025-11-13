0d21bc34eeb9787c064f179386b206acfacee04c
BarkerZlawg <kmendez6094@floridapoly.edu>
2025-11-13 00:14:18 +0000

    The player-input code for the game

--- game_folder/game.py
+++ game_folder/game.py
@@ -1,1 +1,9 @@
 print("Welcome to Rock-Paper-Scissors!")
+while True:
+    user_input = input("Choose rock, paper, or scissors: ").lower()
+    if user_input in ["rock", "paper", "scissors"]:
+        print("You chose:", user_input)
+        break
+    else:
+        print("Your input was invalid.")
+        pass
